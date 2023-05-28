8a41dc34715a2163d32acae5e58992efce2b9d26
lllhjf <lllhjf@jaist.ac.jp>
2023-05-28 13:15:55 +0800

    Add files via upload

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9603303ead6f4aa18073b9169ab82427',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '0bec75f0-0a11-420d-a3e0-b8beb23560c0',
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'minei.us.org',
+  author: 'Jinfeng Huang',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'lllhjf',
+  github: 'lllhjf',
+  linkedin: 'lllhjf',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +50,24 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Experiences',
+      pageId: 'a49b87d5e4224bc184b8027e727047dc'
+    },
+    {
+      title: 'Publications',
+      pageId: 'c38a4c6cc78a4e589ce8ff977e80e8c6'
+    },
+    {
+      title: 'About',
+      pageId: '5c01b733b3ed48d3bf71754a504e6e67'
+    },
+    {
+      title: 'Contact',
+      pageId: '5bb1fb0b46914459b5cf1f4245d95764'
+    },
+  ]
 })
